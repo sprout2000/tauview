a577a2d85d2497defd832090f85ae8d1c7e9b985
sprout2000 <wassabie64@gmail.com>
2022-04-08 17:08:23 +0900

    chore: Created menu/locales.rs.

--- src-tauri/src/menu.rs
+++ src-tauri/src/menu.rs
@@ -1,66 +1,14 @@
-use json_gettext::{get_text, static_json_gettext_build};
+use json_gettext::get_text;
 use sys_locale::get_locale;
 use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
 
+mod locales;
+
 pub fn default() -> Menu {
+    let ctx = locales::default();
     let locale = get_locale()
         .unwrap_or_else(|| String::from("en-US"))
         .replace('_', "-");
-
-    let ctx = static_json_gettext_build!(
-        "en-US";
-        "ar-AE" => "locales/ar.json",
-        "ar-BH" => "locales/ar.json",
-        "ar-DZ" => "locales/ar.json",
-        "ar-EG" => "locales/ar.json",
-        "ar-KW" => "locales/ar.json",
-        "ar-IQ" => "locales/ar.json",
-        "ar-JO" => "locales/ar.json",
-        "ar-LB" => "locales/ar.json",
-        "ar-MA" => "locales/ar.json",
-        "ar-OM" => "locales/ar.json",
-        "ar-QA" => "locales/ar.json",
-        "ar-SA" => "locales/ar.json",
-        "ar-TN" => "locales/ar.json",
-        "ar-YE" => "locales/ar.json",
-        "cs-CZ" => "locales/cs.json",
-        "de-AT" => "locales/de.json",
-        "de-CH" => "locales/de.json",
-        "de-DE" => "locales/de.json",
-        "de-LI" => "Locales/de.json",
-        "de-LU" => "Locales/de.json",
-        "en-GB" => "locales/en.json",
-        "en-US" => "locales/en.json",
-        "es-AR" => "locales/es.json",
-        "es-BO" => "locales/es.json",
-        "es-CL" => "locales/es.json",
-        "es-CO" => "locales/es.json",
-        "es-CR" => "locales/es.json",
-        "es-DO" => "locales/es.json",
-        "es-EC" => "locales/es.json",
-        "es-ES" => "locales/es.json",
-        "es-GT" => "locales/es.json",
-        "es-HN" => "locales/es.json",
-        "es-MX" => "locales/es.json",
-        "es-NI" => "locales/es.json",
-        "es-PA" => "locales/es.json",
-        "es-PE" => "locales/es.json",
-        "es-PR" => "locales/es.json",
-        "es-PY" => "locales/es.json",
-        "es-SV" => "locales/es.json",
-        "es-US" => "locales/es.json",
-        "es-UY" => "locales/es.json",
-        "es-VE" => "locales/es.json",
-        "hu-HU" => "locales/hu.json",
-        "ja-JP" => "locales/ja.json",
-        "pl-PL" => "locales/pl.json",
-        "pt-BR" => "locales/pt.json",
-        "pt-PT" => "locales/pt.json",
-        "ru-RU" => "locales/ru.json",
-        "zh-CN" => "locales/zh_CN.json",
-        "zh-TW" => "locales/zh_TW.json"
-    )
-    .unwrap();
 
     #[cfg(target_os = "macos")]
     let app_ctx = tauri::generate_context!();
