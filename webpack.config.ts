420931af54102978beb3736ca51b2330e482c783
sprout2000 <wassabie64@gmail.com>
2022-04-08 16:58:30 +0900

    chore: Removed favicon.ico.

--- webpack.config.ts
+++ webpack.config.ts
@@ -31,7 +31,6 @@ const config: Configuration = {
         process.argv[process.argv.length - 1] === 'development'
           ? './src/index.dev.html'
           : './src/index.html',
-      favicon: './src/favicon.ico',
     }),
   ],
   stats: 'minimal',
