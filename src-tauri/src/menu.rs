ba703a86630e8aa141c3bc3d5497adf09c04f9e5
sprout2000 <wassabie64@gmail.com>
2022-04-20 09:05:01 +0900

    fix: Removed window menu on win32.

--- src-tauri/src/menu.rs
+++ src-tauri/src/menu.rs
@@ -1,6 +1,19 @@
 use tauri::{CustomMenuItem, Menu, MenuItem, Submenu};
 
 pub fn default() -> Menu {
+    let file_menu = Submenu::new(
+        "File",
+        Menu::new()
+            .add_item(CustomMenuItem::new("open", "Open...").accelerator("CmdOrCtrl+O"))
+            .add_native_item(MenuItem::Separator)
+            .add_native_item(MenuItem::CloseWindow),
+    );
+
+    let help_menu = Submenu::new(
+        "Help",
+        Menu::new().add_item(CustomMenuItem::new("support", "Support URL...")),
+    );
+
     #[cfg(target_os = "macos")]
     let app_ctx = tauri::generate_context!();
 
@@ -20,14 +33,7 @@ pub fn default() -> Menu {
             .add_native_item(MenuItem::Quit),
     );
 
-    let file_menu = Submenu::new(
-        "File",
-        Menu::new()
-            .add_item(CustomMenuItem::new("open", "Open...").accelerator("CmdOrCtrl+O"))
-            .add_native_item(MenuItem::Separator)
-            .add_native_item(MenuItem::CloseWindow),
-    );
-
+    #[cfg(target_os = "macos")]
     let window_menu = Submenu::new(
         "Window",
         Menu::new()
@@ -35,11 +41,6 @@ pub fn default() -> Menu {
             .add_native_item(MenuItem::Zoom)
             .add_native_item(MenuItem::Separator)
             .add_native_item(MenuItem::EnterFullScreen),
-    );
-
-    let help_menu = Submenu::new(
-        "Help",
-        Menu::new().add_item(CustomMenuItem::new("support", "Support URL...")),
     );
 
     #[cfg(target_os = "macos")]
@@ -50,10 +51,7 @@ pub fn default() -> Menu {
         .add_submenu(help_menu);
 
     #[cfg(not(target_os = "macos"))]
-    let menu = Menu::new()
-        .add_submenu(file_menu)
-        .add_submenu(window_menu)
-        .add_submenu(help_menu);
+    let menu = Menu::new().add_submenu(file_menu).add_submenu(help_menu);
 
     menu
 }
