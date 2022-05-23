2ea2f6f68305f49bbfe93294545e5d57c617e54a
sprout2000 <wassabie64@gmail.com>
2022-05-23 09:17:14 +0900

    refactor: Use "mapObj.current?".

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -181,7 +181,7 @@ export const App = () => {
     switch (e.key) {
       case '0':
         e.preventDefault();
-        mapObj.current && mapObj.current.setZoom(0);
+        mapObj.current?.setZoom(0);
         break;
       case e.metaKey && 'ArrowRight':
       case 'j':
