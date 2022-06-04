a277f97f97be6cec3fbedbe3cfdf1646e4094b29
sprout2000 <wassabie64@gmail.com>
2022-06-04 10:54:19 +0900

    chore: Added vite-plugin-html.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
-export default defineConfig({
-  root: './src',
-  build: {
-    outDir: '../dist',
-    minify: true,
-    emptyOutDir: true,
-  },
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const isDev = command === 'serve';
+
+  return {
+    root: './src',
+    build: {
+      outDir: '../dist',
+      minify: true,
+      emptyOutDir: true,
+    },
+    plugins: [
+      react(),
+      createHtmlPlugin({
+        minify: !isDev,
+        inject: {
+          data: {
+            devtools: isDev
+              ? `<script src="http://localhost:8097"></script>`
+              : undefined,
+          },
+        },
+      }),
+    ],
+  };
 });
