fbbb1cce9fb859ff1a9b74f307f918b96ac32b4a
sprout2000 <wassabie64@gmail.com>
2022-06-30 14:05:43 +0900

    fix: Added Window menu for win32.

--- src-tauri/src/menu.rs
+++ src-tauri/src/menu.rs
@@ -14,7 +14,6 @@ fn get_fullscreen_accelerator() -> String {
 
 pub fn default(app_context: &Context<EmbeddedAssets>) -> Menu {
     let locale = get_locale().unwrap_or_else(|| String::from("en-US"));
-
     let ctx = static_json_gettext_build!(
         "en-US";
         "ar-AE" => "locales/ar.json",
@@ -76,6 +75,22 @@ pub fn default(app_context: &Context<EmbeddedAssets>) -> Menu {
     )
     .unwrap();
 
+    #[cfg(target_os = "macos")]
+    let app_menu = Submenu::new(
+        &app_context.package_info().name,
+        Menu::new()
+            .add_native_item(MenuItem::About(
+                app_context.package_info().name.clone(),
+                tauri::AboutMetadata::new(),
+            ))
+            .add_native_item(MenuItem::Separator)
+            .add_native_item(MenuItem::Hide)
+            .add_native_item(MenuItem::HideOthers)
+            .add_native_item(MenuItem::ShowAll)
+            .add_native_item(MenuItem::Separator)
+            .add_native_item(MenuItem::Quit),
+    );
+
     let file_menu = Submenu::new(
         get_text!(ctx, &locale, "File").unwrap().to_string(),
         Menu::new()
@@ -122,6 +137,17 @@ pub fn default(app_context: &Context<EmbeddedAssets>) -> Menu {
             ),
     );
 
+    #[cfg(target_os = "macos")]
+    let help_menu = Submenu::new(
+        get_text!(ctx, &locale, "Help").unwrap().to_string(),
+        Menu::new().add_item(CustomMenuItem::new(
+            "support",
+            get_text!(ctx, &locale, "Support URL...")
+                .unwrap()
+                .to_string(),
+        )),
+    );
+
     #[cfg(not(target_os = "macos"))]
     let help_menu = Submenu::new(
         get_text!(ctx, &locale, "Help").unwrap().to_string(),
@@ -140,33 +166,6 @@ pub fn default(app_context: &Context<EmbeddedAssets>) -> Menu {
     );
 
     #[cfg(target_os = "macos")]
-    let help_menu = Submenu::new(
-        get_text!(ctx, &locale, "Help").unwrap().to_string(),
-        Menu::new().add_item(CustomMenuItem::new(
-            "support",
-            get_text!(ctx, &locale, "Support URL...")
-                .unwrap()
-                .to_string(),
-        )),
-    );
-
-    #[cfg(target_os = "macos")]
-    let app_menu = Submenu::new(
-        &app_context.package_info().name,
-        Menu::new()
-            .add_native_item(MenuItem::About(
-                app_context.package_info().name.clone(),
-                tauri::AboutMetadata::new(),
-            ))
-            .add_native_item(MenuItem::Separator)
-            .add_native_item(MenuItem::Hide)
-            .add_native_item(MenuItem::HideOthers)
-            .add_native_item(MenuItem::ShowAll)
-            .add_native_item(MenuItem::Separator)
-            .add_native_item(MenuItem::Quit),
-    );
-
-    #[cfg(target_os = "macos")]
     let menu = Menu::new()
         .add_submenu(app_menu)
         .add_submenu(file_menu)
@@ -174,7 +173,10 @@ pub fn default(app_context: &Context<EmbeddedAssets>) -> Menu {
         .add_submenu(help_menu);
 
     #[cfg(not(target_os = "macos"))]
-    let menu = Menu::new().add_submenu(file_menu).add_submenu(help_menu);
+    let menu = Menu::new()
+        .add_submenu(file_menu)
+        .add_submenu(window_menu)
+        .add_submenu(help_menu);
 
     menu
 }
