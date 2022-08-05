68726265a0e31a148030d2921ca72a414516df1e
sprout2000 <wassabie64@gmail.com>
2022-08-06 08:43:37 +0900

    chore: Reverted vite-plugin-html.

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,40 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
-export default defineConfig({
-  root: './src',
-  build: {
-    outDir: '../dist',
-    minify: true,
-    emptyOutDir: true,
-  },
-  test: {
-    cache: false,
-    globals: true,
-    environment: 'jsdom',
-    coverage: {
-      enabled: true,
-      reporter: ['text', 'json-summary'],
-      reportsDirectory: '../coverage',
+export default defineConfig(({ command }) => {
+  const isDev = command === 'serve';
+
+  return {
+    root: './src',
+    build: {
+      outDir: '../dist',
+      minify: true,
+      emptyOutDir: true,
     },
-  },
-  plugins: [react()],
+    test: {
+      cache: false,
+      globals: true,
+      environment: 'jsdom',
+      coverage: {
+        enabled: true,
+        reporter: ['text', 'json-summary'],
+        reportsDirectory: '../coverage',
+      },
+    },
+    plugins: [
+      react(),
+      createHtmlPlugin({
+        minify: !isDev,
+        inject: {
+          data: {
+            devtools: isDev
+              ? '<script src="http://localhost:8097"></script>'
+              : undefined,
+          },
+        },
+      }),
+    ],
+  };
 });
