00254a48e16fda6006eb98d45dd84b6691c096cb
sprout2000 <wassabie64@gmail.com>
2022-08-28 09:52:47 +0900

    refactor: Use "as Element".

--- src/index.tsx
+++ src/index.tsx
@@ -12,8 +12,7 @@ const locale =
 
 setLocales(locale);
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as Element).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
