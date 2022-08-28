a94cf003838367f994b523c4b4358b998a79280d
sprout2000 <wassabie64@gmail.com>
2022-08-28 09:43:51 +0900

    chore: Removed vite-plugin-html.

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,22 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { createHtmlPlugin } from 'vite-plugin-html';
 
-export default defineConfig(({ command }) => {
-  const isDev = command === 'serve';
-
-  return {
-    root: './src',
-    build: {
-      outDir: '../dist',
-      minify: true,
-      emptyOutDir: true,
+export default defineConfig({
+  root: './src',
+  build: {
+    outDir: '../dist',
+    minify: true,
+    emptyOutDir: true,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    coverage: {
+      enabled: true,
+      reporter: ['text'],
+      reportsDirectory: '../coverage',
     },
-    test: {
-      cache: false,
-      globals: true,
-      environment: 'jsdom',
-      coverage: {
-        enabled: true,
-        reporter: ['text', 'json-summary'],
-        reportsDirectory: '../coverage',
-      },
-    },
-    plugins: [
-      react(),
-      createHtmlPlugin({
-        minify: !isDev,
-        inject: {
-          data: {
-            devtools: isDev
-              ? '<script src="http://localhost:8097"></script>'
-              : undefined,
-          },
-        },
-      }),
-    ],
-  };
+  },
+  plugins: [react()],
 });
