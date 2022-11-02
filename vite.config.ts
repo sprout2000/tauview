ce0b6b29dc7d96d3130cad70cf12bb66e68a9fd7
sprout2000 <wassabie64@gmail.com>
2022-11-02 16:38:01 +0900

    chore: Removed vitest temporarily.

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { createHtmlPlugin } from 'vite-plugin-html';
@@ -12,15 +11,6 @@ export default defineConfig(({ command }) => {
       outDir: '../dist',
       minify: true,
       emptyOutDir: true,
-    },
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      coverage: {
-        enabled: true,
-        reporter: ['text'],
-        reportsDirectory: '../coverage',
-      },
     },
     plugins: [
       react(),
