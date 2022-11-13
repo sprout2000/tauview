090554738c58070a08b7e089263b3e825c5ba041
sprout2000 <wassabie64@gmail.com>
2022-11-13 09:34:44 +0900

    style: Updated setLocales.ts.

--- src/setLocales.ts
+++ src/setLocales.ts
@@ -1,38 +1,18 @@
 import i18next from 'i18next';
 
-import en from '../src-tauri/locales/en.json';
-import ja from '../src-tauri/locales/ja.json';
-import zh_TW from '../src-tauri/locales/zh_TW.json';
-/** Merge the pull request sent by PetrTodorov. */
-/** https://github.com/sprout2000/leafview/pull/68 */
-import cs from '../src-tauri/locales/cs.json';
-/** Merge the pull request sent by DrDeee. */
-/** https://github.com/sprout2000/leafview/pull/166 */
-import de from '../src-tauri/locales/de.json';
-/** Merge the pull request sent by singuerinc */
-/** https://github.com/sprout2000/leafview/pull/178 */
-import es from '../src-tauri/locales/es.json';
-/** Merge the pull request sent by nukeop */
-/** https://github.com/sprout2000/leafview/pull/214 */
-import pl from '../src-tauri/locales/pl.json';
-/** Merge the pull request sent by kitt3911 */
-/** https://github.com/sprout2000/leafview/pull/215 */
-import ru from '../src-tauri/locales/ru.json';
-/** Merge the pull request sent by guaycuru */
-/** https://github.com/sprout2000/leafview/pull/232 */
-import pt from '../src-tauri/locales/pt.json';
-/** Merge the pull request sent by ArcherGu */
-/** https://github.com/sprout2000/leafview/pull/235 */
-import zh_CN from '../src-tauri/locales/zh_CN.json';
-/** Merge the pull request sent by SuhaibAtef */
-/** https://github.com/sprout2000/leafview/pull/274 */
 import ar from '../src-tauri/locales/ar.json';
-/** Merge the pull request sent by Levminer */
-/** https://github.com/sprout2000/leafview/pull/305 */
-import hu from '../src-tauri/locales/hu.json';
-/** Merge the pull request sent by BackSpace54 */
-/** https://github.com/sprout2000/leafview/pull/312 */
+import cs from '../src-tauri/locales/cs.json';
+import de from '../src-tauri/locales/de.json';
+import en from '../src-tauri/locales/en.json';
+import es from '../src-tauri/locales/es.json';
 import fr from '../src-tauri/locales/fr.json';
+import hu from '../src-tauri/locales/hu.json';
+import ja from '../src-tauri/locales/ja.json';
+import pl from '../src-tauri/locales/pl.json';
+import pt from '../src-tauri/locales/pt.json';
+import ru from '../src-tauri/locales/ru.json';
+import zh_CN from '../src-tauri/locales/zh_cn.json';
+import zh_TW from '../src-tauri/locales/zh_tw.json';
 
 export const setLocales = (locale: string) => {
   i18next.init({
