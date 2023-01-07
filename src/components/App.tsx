031f9851a8a1b7cc183724a3df88659a76d12e54
sprout2000 <wassabie64@gmail.com>
2023-01-07 13:17:28 +0900

    refactor: Create View & Grid.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,21 +1,14 @@
-import {
-  Fragment,
-  useCallback,
-  useEffect,
-  useLayoutEffect,
-  useRef,
-  useState,
-} from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import { event } from '@tauri-apps/api';
+import { invoke } from '@tauri-apps/api/tauri';
 import { dirname } from '@tauri-apps/api/path';
 import { getCurrent } from '@tauri-apps/api/window';
-import { convertFileSrc, invoke } from '@tauri-apps/api/tauri';
 
-import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
-
+import { View } from './View';
+import { Grid } from './Grid';
 import { ToolBar } from './ToolBar';
+
 import './App.scss';
 
 type Payload = {
@@ -27,80 +20,12 @@ export const App = () => {
   const [grid, setGrid] = useState(false);
   const [imgList, setImgList] = useState<string[]>([]);
 
-  const mapRef = useRef<HTMLDivElement>(null);
-  const currentRef = useRef<HTMLImageElement>(null);
-  const mapObj: React.MutableRefObject<L.Map | null> = useRef(null);
-
-  const isDev = process.env.NODE_ENV === 'development';
-
   const readDir = useCallback(async () => {
     const dir = await dirname(url);
     const list: string[] = await invoke('get_entries', { dir });
 
     return list;
   }, [url]);
-
-  const getZoom = useCallback(
-    (iw: number, width: number, ih: number, height: number) => {
-      if (iw > width || ih > height) {
-        const zoomX = width / iw;
-        const zoomY = height / ih;
-
-        return zoomX >= zoomY ? zoomY : zoomX;
-      } else {
-        return 1;
-      }
-    },
-    []
-  );
-
-  const draw = useCallback(
-    (width: number, height: number) => {
-      const node = mapRef.current;
-
-      if (node) {
-        const image = new Image();
-        image.onload = () => {
-          const zoom = getZoom(image.width, width, image.height, height);
-
-          const bounds = new L.LatLngBounds([
-            [image.height * zoom, 0],
-            [0, image.width * zoom],
-          ]);
-
-          mapObj.current?.off();
-          mapObj.current?.remove();
-
-          mapObj.current = L.map(node, {
-            maxBounds: bounds,
-            crs: L.CRS.Simple,
-            preferCanvas: true,
-            zoomSnap: 0.3,
-            zoomDelta: 0.3,
-            wheelPxPerZoomLevel: 360,
-            doubleClickZoom: false,
-            zoomControl: false,
-            attributionControl: false,
-          }).fitBounds(bounds);
-
-          mapObj.current.on('dblclick', () => {
-            mapObj.current?.setView(bounds.getCenter(), 0);
-          });
-
-          if (image.width < width && image.height < height) {
-            mapObj.current.setView(bounds.getCenter(), 0, { animate: false });
-          }
-
-          L.imageOverlay(image.src, bounds).addTo(mapObj.current);
-
-          node.blur();
-          node.focus();
-        };
-        image.src = url ? convertFileSrc(url) : '';
-      }
-    },
-    [url, getZoom]
-  );
 
   const preventDefault = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
@@ -234,10 +159,6 @@ export const App = () => {
     if (!url || grid) return;
 
     switch (e.key) {
-      case '0':
-        e.preventDefault();
-        mapObj.current?.setZoom(0);
-        break;
       case e.metaKey && 'ArrowRight':
       case e.ctrlKey && 'n':
         e.preventDefault();
@@ -334,28 +255,6 @@ export const App = () => {
     }
   }, [url]);
 
-  useEffect(() => {
-    const resizeObserver = new ResizeObserver((entries) => {
-      const width = entries[0].contentRect.width;
-      const height = entries[0].contentRect.height;
-      draw(width, height);
-    });
-
-    mapRef.current && resizeObserver.observe(mapRef.current);
-
-    return () => {
-      resizeObserver.disconnect();
-    };
-  }, [draw, grid]);
-
-  useLayoutEffect(() => {
-    if (grid)
-      currentRef.current?.scrollIntoView({
-        behavior: 'smooth',
-        block: 'center',
-      });
-  }, [grid]);
-
   return (
     <div
       data-testid="container"
@@ -365,31 +264,17 @@ export const App = () => {
       onDragOver={preventDefault}
       onDragEnter={preventDefault}
       onDragLeave={preventDefault}
-      onContextMenu={isDev ? undefined : preventDefault}
+      onContextMenu={preventDefault}
     >
       {grid ? (
-        <div
-          className="thumb-container"
-          onClick={onClickBlank}
-          onKeyDown={(e) => {
-            e.preventDefault();
-          }}
-        >
-          {imgList.map((item) => (
-            <img
-              key={item}
-              src={convertFileSrc(item)}
-              ref={item === url ? currentRef : null}
-              className={item === url ? 'thumb current' : 'thumb'}
-              onClick={(e) => onClickThumb(e, item)}
-              onDragStart={() => {
-                return false;
-              }}
-            />
-          ))}
-        </div>
+        <Grid
+          url={url}
+          imgList={imgList}
+          onClickBlank={onClickBlank}
+          onClickThumb={onClickThumb}
+        />
       ) : (
-        <Fragment>
+        <>
           <ToolBar
             onOpen={onOpen}
             onPrev={onPrev}
@@ -397,8 +282,8 @@ export const App = () => {
             onRemove={onRemove}
             onToggleGrid={onToggleGrid}
           />
-          <div className={url ? 'view' : 'view init'} ref={mapRef} />
-        </Fragment>
+          <View url={url} />
+        </>
       )}
     </div>
   );
---
+++ src/components/Grid.tsx
@@ -0,0 +1,47 @@
+import { memo, useLayoutEffect, useRef } from 'react';
+import { convertFileSrc } from '@tauri-apps/api/tauri';
+
+import './Grid.scss';
+
+type Props = {
+  url: string;
+  imgList: string[];
+  onClickBlank: () => Promise<void>;
+  onClickThumb: (
+    e: React.MouseEvent<HTMLImageElement, MouseEvent>,
+    item: string
+  ) => Promise<void>;
+};
+
+export const Grid = memo((props: Props) => {
+  const currentRef = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    currentRef.current?.scrollIntoView({
+      behavior: 'smooth',
+      block: 'center',
+    });
+  }, []);
+
+  return (
+    <div className="thumb-container" onClick={props.onClickBlank}>
+      {props.imgList.map((item) => (
+        <div
+          className="wrapper"
+          key={item}
+          ref={item === props.url ? currentRef : null}
+        >
+          <img
+            src={convertFileSrc(item)}
+            className={item === props.url ? 'thumb current' : 'thumb'}
+            onClick={(e) => props.onClickThumb(e, item)}
+            onDragStart={() => {
+              return false;
+            }}
+          />
+        </div>
+      ))}
+    </div>
+  );
+});
+Grid.displayName = 'Grid';
