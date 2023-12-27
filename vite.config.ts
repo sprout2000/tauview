3666835cb7c2f8d64ab4f15e184681b3e1111ec6
sprout2000 <wassabie64@gmail.com>
2023-12-28 08:46:54 +0900

    chore: Update vite to v5.x

--- vite.config.ts
+++ vite.config.ts
@@ -23,15 +23,15 @@ export default defineConfig(async ({ command }) => ({
   server: {
     port: 1420,
     strictPort: true,
+    watch: {
+      ignored: ["**/src-tauri/**"],
+    },
   },
-  envPrefix: ["VITE_", "TAURI_"],
   root: "./src",
   publicDir: "../public",
   build: {
     outDir: "../dist",
     emptyOutDir: true,
-    target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
-    minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
     sourcemap: !!process.env.TAURI_DEBUG,
   },
   test: {
